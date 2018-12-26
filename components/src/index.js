03cc612cd038588442601a3a319d3e0c54a001f7
ayslepnev <ayslepnev@domclick.ru>
2018-12-26 08:38:12 +0300

    REACT: Props

---
+++ components/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <CommentDetail></CommentDetail>
+      <CommentDetail></CommentDetail>
+      <CommentDetail></CommentDetail>
+      <CommentDetail></CommentDetail>
+    </div>
+  )
+};
+
+ReactDOM.render(
+  <App/>,
+  document.querySelector('#root')
+);
