91833619c731218b91e8100da5f3c7e734e3fad6
ayslepnev <ayslepnev@domclick.ru>
2019-01-05 11:09:02 +0300

    REACT: pics

---
+++ 4-pics/src/components/SearchBar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
+
+  render() {
+    return (
+      <div className="ui segment">
+        <form action="" className="ui form">
+          <div className="field">
+            <label htmlFor="search">Image Search</label>
+            <input id="search" type="text" onChange={this.onInputChange} />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
