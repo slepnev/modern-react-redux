a1415287f262770bbce9f676ff5a1b65856663b4
ayslepnev <ayslepnev@domclick.ru>
2019-01-28 08:35:42 +0300

    REACT: redux

--- 7-redux-songs/src/components/App.js
+++ 7-redux-songs/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import SongList from './SongList';
+import SongDetail from './SongDetail';
 
 class App extends Component {
   render() {
@@ -7,7 +8,10 @@ class App extends Component {
       <div className="ui container grid">
         <div className="ui row">
           <div className="column eight wide">
-            <SongList/>
+            <SongList />
+          </div>
+          <div className="column eight wide">
+            <SongDetail />
           </div>
         </div>
       </div>
---
+++ 8-redux-api-blog/src/components/PostList.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
+
+class PostList extends Component {
+
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
+  render() {
+    return (
+      <div>
+        PostList
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+
+  };
+};
+
+export default connect(null, { fetchPosts })(PostList);
