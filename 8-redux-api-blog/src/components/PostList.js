e19b13ed31d344c27a63ac6b4c0a28bfd151763c
ayslepnev <ayslepnev@domclick.ru>
2019-01-30 08:35:25 +0300

    REACT: redux-thunk

--- 8-redux-api-blog/src/components/PostList.js
+++ 8-redux-api-blog/src/components/PostList.js
@@ -8,10 +8,26 @@ class PostList extends Component {
     this.props.fetchPosts();
   }
 
+  renderList() {
+    return this.props.posts.map(post => {
+      return (
+        <div className="item" key={post.id}>
+          <i className="large middle aligned icon user"/>
+          <div className="content">
+            <div className="description">
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        </div>
+      );
+    });
+  }
+
   render() {
     return (
-      <div>
-        PostList
+      <div className="ui relaxed divided list">
+        {this.renderList()}
       </div>
     );
   }
@@ -19,8 +35,8 @@ class PostList extends Component {
 
 const mapStateToProps = state => {
   return {
-
+    posts: state.posts
   };
 };
 
-export default connect(null, { fetchPosts })(PostList);
+export default connect(mapStateToProps, { fetchPosts })(PostList);
