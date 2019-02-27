fdf363cd49c84deaa51c65bf41872bd608dcf4d2
ayslepnev <ayslepnev@domclick.ru>
2019-02-27 08:35:01 +0300

    REACT: server

--- 9-streams/client/src/index.js
+++ 9-streams/client/src/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
+import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
 
+
 const composeEnchancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers, composeEnchancers(applyMiddleware()));
+const store = createStore(
+  reducers,
+  composeEnchancers(applyMiddleware(reduxThunk))
+);
 
 ReactDOM.render(
   <Provider store={store}>
