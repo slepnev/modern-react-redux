4099944a90cf0fb9666f10a1b2f2f563ec143b6a
ayslepnev <ayslepnev@domclick.ru>
2019-02-28 08:35:51 +0300

    REACT: rest

--- 9-streams/client/src/reducers/index.js
+++ 9-streams/client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form'
 import authResucer from './authResucer';
+import streamReducer from './streamReducer';
 
 export default combineReducers({
   auth: authResucer,
-  form: formReducer
+  form: formReducer,
+  streams: streamReducer
 });
