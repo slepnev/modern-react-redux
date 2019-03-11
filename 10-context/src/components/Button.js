5472a6070ba4d3ccc60cd9cf3ada274ac7c62508
ayslepnev <ayslepnev@domclick.ru>
2019-03-11 08:32:35 +0300

    REACT: context

---
+++ 10-context/src/components/Button.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import LanguageContext from '../context/LanguageContext';
+
+class Button extends Component {
+  static contextType = LanguageContext;
+
+  render() {
+    const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
+
+    return (
+      <button className="ui button primary">
+        {text}
+      </button>
+    );
+  }
+}
+
+
+
+export default Button;
